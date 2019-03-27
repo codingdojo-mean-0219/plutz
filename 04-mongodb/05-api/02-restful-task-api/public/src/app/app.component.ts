caaff40cba94c68fa1f724699896ab8881ede99d
Peter Lutz <pj.lutz17@gmail.com>
2019-03-27 09:38:46 -0400

    added observable to component

--- 04-mongodb/05-api/02-restful-task-api/public/src/app/app.component.ts
+++ 04-mongodb/05-api/02-restful-task-api/public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,10 +6,23 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title="this is the title";
-
+export class AppComponent implements OnInit{
+  title="Restful Tasks";
+  tasks=[];
   constructor(private _httpService: HttpService){
+  }
+  ngOnInit(){
+    this.getTasksFromService();
 
+  }
+  getTasksFromService(){
+    let observable =this._httpService.getTasks();
+    observable.subscribe(data=>{
+      console.log("Got the Tasks!",data)
+      for (const task in data) {
+        this.tasks.push(data[task]);
+      }
+    })
+    
   }
 }
--- 04-mongodb/05-api/02-restful-task-api/public/src/app/http.service.ts
+++ 04-mongodb/05-api/02-restful-task-api/public/src/app/http.service.ts
@@ -5,15 +5,15 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
 
   constructor(private _http:HttpClient) {
-    this.getTasks();
+    // this.getTasks();
    }
    getTasks(){
-     let tempObservable = this._http.get('/tasks');
-     tempObservable.subscribe(data=>console.log("Got the tasks",data));
+    return this._http.get('/tasks');
+    
    }
 
    updateTasks(taskID){
-     let tempObservable=this._http.get('/tasks/'+taskID);
-     tempObservable.subscribe(data=>console.log("Got task",data));
+     return this._http.get('/tasks/'+taskID);
+     
    }
 }
